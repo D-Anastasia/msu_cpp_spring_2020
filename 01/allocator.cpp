8d874908ca17cc61f31959c42f2c717fd8c0eea7
D-Anastasia <61876117+D-Anastasia@users.noreply.github.com>
2020-03-10 00:22:43 +0300

    Add files via upload

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -7,18 +7,26 @@ Allocator::Allocator(){
 	memory = NULL;
 }
 void Allocator::makeAllocator(size_t maxSize){
-	char* memory1 = new char[maxSize];
+	char* memory1;
+	try{
+		memory1 = new char[maxSize];
+		if (memory1 == NULL){
+			throw "memory error";
+		}
+	} catch (char* str){
+		cout << str << endl;
+	}
 	memory = memory1;
 	max_size = maxSize;
 	allocated = 0;
 }
 
 char* Allocator::alloc(size_t size){
-	if(allocated + size > max_size){
+	if (allocated + size > max_size){
 		return NULL;
 	} else {
 		allocated += size;
-		return (char*)memory+allocated;
+		return (char*)memory + allocated - size;
 	}
 }
 
--- 01/test.cpp
+++ 01/test.cpp
@@ -4,31 +4,30 @@
 #include "allocator.hpp"
 using namespace std;
 int main(void){
-	Allocator* A = new Allocator();
-	A->makeAllocator(20);
+	Allocator A;
+	A.makeAllocator(20);
 
-	char* p = A->alloc(10);
+	char* p = A.alloc(10);
 	if (p==NULL) cout<<"ERR: p=NULL"<<endl;
-	p = A->alloc(9);
-	if (p==NULL) cout<<"ERR: p=NULL"<<endl;
-
-	p = A->alloc(1);
+	p = A.alloc(9);
 	if (p==NULL) cout<<"ERR: p=NULL"<<endl;
 
-	p = A->alloc(1);
+	p = A.alloc(1);
+	if (p==NULL) cout<<"ERR: p=NULL"<<endl;
+
+	p = A.alloc(1);
 	if (p!=NULL) cout<<"ERR: p!=NULL"<<endl;
 
-	A->reset();
+	A.reset();
 
-	p = A->alloc(10);
+	p = A.alloc(10);
 	if (p==NULL) cout<<"ERR: p=NULL"<<endl;
 
-	p = A->alloc(10);
+	p = A.alloc(10);
 	if (p==NULL) cout<<"ERR: p=NULL"<<endl;
 
-	p = A->alloc(1);
+	p = A.alloc(1);
 	if (p!=NULL) cout<<"ERR: p!=NULL"<<endl;
 
-	delete A;
 	return 0;
 }
